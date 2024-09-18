738fc7eac5b0d4db6437149f9ff26449079c1757
Karunakar Gadireddy <karunakar.gadireddy@sia-partners.com>
2024-09-18 13:17:52 -0400

    setup project

---
+++ dAIshboard/api/api.py
@@ -0,0 +1,34 @@
+import time
+import pandas as pd
+import plotly
+import plotly.express as px
+from flask import Flask
+
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
+
+@app.route('/plot')
+def plot_test():
+    df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="stack",title="Test")
+    fig.update_layout(title_x=0.5)
+    graphJSON = plotly.io.to_json(fig, pretty=True)
+    return graphJSON
